a41598dcdc69d52e3a8cefa1f014b3b3ab4f9127
Shawn Wilkinson <shawnwilkinson11@gmail.com>
2015-10-13 09:13:30 -0500

    heroku debugging

--- app/models/yelp_interface.rb
+++ app/models/yelp_interface.rb
@@ -1,20 +1,20 @@
 class YelpInterface < ActiveRecord::Base
 
-    @@client = Yelp::Client.new({ consumer_key: yelp_consumer_key,
-                            consumer_secret: yelp_consumer_secret,
-                            token: yelp_token,
-                            token_secret: yelp_token_secret
-                          })
+    # @@client = Yelp::Client.new({ consumer_key: yelp_consumer_key,
+    #                         consumer_secret: yelp_consumer_secret,
+    #                         token: yelp_token,
+    #                         token_secret: yelp_token_secret
+    #                       })
 
 
-    def self.make_search_call(terms,location='Chicago',limit=4)
-      params = {limit:limit.to_i,location:location}
-      @@client.search(terms,params)
-    end
+    # def self.make_search_call(terms,location='Chicago',limit=4)
+    #   params = {limit:limit.to_i,location:location}
+    #   @@client.search(terms,params)
+    # end
 
-    def self.make_business_call
+    # def self.make_business_call
 
-    end
+    # end
 
 
 end
